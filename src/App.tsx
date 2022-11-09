e5b3e8c1ee21e7ae1c47f2ba6ff182bcfc39507f
yong <tkddbs1644@gmail.com>
2022-11-09 16:54:44 +0900

    feat: 검색바 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
 
 function App() {
-  return <h1>Hello typescript react!</h1>;
+  const [keyword, setKeyword] = useState("");
+
+  const handleChangeKeyword = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.currentTarget.value);
+  };
+
+  return (
+    <>
+      <h1>국내 모든 임상시험 검색하고 온라인으로 참여하기</h1>
+      <input
+        name="keyword"
+        placeholder="질환명을 입력해 주세요."
+        value={keyword}
+        onChange={handleChangeKeyword}
+        autoComplete="off"
+      />
+    </>
+  );
 }
 
 export default App;
